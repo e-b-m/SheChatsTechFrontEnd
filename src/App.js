44fbe833c79e5664ccb9430921ca522e8adbf540
Emma Bayley-Melendez <e.melendez98@outlook.com>
2023-06-16 10:28:56 +0100

    added to app.js logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
+import SheChatsTech from "/Users/emma/Documents/she_chats_tech/src/images/She Chats Tech.png";
 
 function App() {
   return (
@@ -8,7 +9,8 @@ function App() {
         <ul class="nav-list">
           <div class="logo">
             <img
-              src={require(she_chats_tech / src / images / SheChatsTech.png)}
+              src={require("/Users/emma/Documents/she_chats_tech/src/images/She Chats Tech.png")}
+              alt="She Chats Tech"
             ></img>
           </div>
         </ul>
