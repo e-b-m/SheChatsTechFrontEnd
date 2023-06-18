e7ff20df6dc5b499bf206772260554ed3e68ef3a
Emma Bayley-Melendez <e.melendez98@outlook.com>
2023-06-18 18:26:18 +0100

    added founder image and styled on index.css

--- src/components/People.js
+++ src/components/People.js
@@ -4,6 +4,7 @@ const People = () => {
   return (
     <div id="founder-picture">
       <img src={require("../images/people-founder.png")}></img>
+      <h2 id="founder">Founder Emma Bayley-Melendez</h2>
     </div>
   );
 };
