bf3766d59885411e6008b44d40618795c41e26c9
Emma Bayley-Melendez <e.melendez98@outlook.com>
2023-06-18 20:31:18 +0100

    footer links added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,21 @@ const Footer = () => {
   return (
     <footer>
       <ul id="footer-elements">
-        <li>Contact </li>
+        {/* <li>Contact </li>
         <li>About</li>
-        <li>FAQs</li>
+        <li>FAQs</li> */}
+
+        <li>
+          <a href="#">Contact</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">FAQs</a>
+        </li>
       </ul>
+      {/* </ul> */}
     </footer>
   );
 };
