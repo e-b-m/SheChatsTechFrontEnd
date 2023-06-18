64d8627391ae28688e789fb6e10bdb31d0d0408f
Emma Bayley-Melendez <e.melendez98@outlook.com>
2023-06-18 20:05:12 +0100

    styling changes to index.css

--- src/components/People.js
+++ src/components/People.js
@@ -8,15 +8,19 @@ const People = () => {
       <p id="about-em">
         Emma founded She Chats Tech after feeling she was lacking an in-person
         women in tech community. Whilst she found there were a range of events
-        being organised the structure could often be more formal and not always
-        lead to finding more long-term connections with other like-minded
-        individuals. With the hope of creating a series of informal in-person
-        events covering a range of topics and for those in the early stages of
-        their tech journey, she wanted to curate a safe space for learning,
-        experience sharing and connection.Emma is a non-tech graduate that
-        embarked on a career in tech after completing a Python course with Code
-        First Girls, she is passionate about diversity in tech and in supporting
-        others as they take their steps into tech.
+        being organised the structure could often be more formal and would not
+        always lead to long-term connections with like-minded individuals. With
+        the hope of creating a series of informal in-person events covering a
+        range of topics and for those in the early stages of their tech journey,
+        she wanted to curate a safe space for learning, experience sharing and
+        connection.Emma is a non-tech graduate that embarked on a career in tech
+        after completing a Python course with Code First Girls. The course left
+        her feeling curious and empowered it was very different to her degree in
+        Politics but she didn't let this stop her interests continuing and
+        decided to apply to the Bright Network Technology Academy Bootcamp.
+        12-weeks after completing she applied to jobs and landed her first tech
+        role as a software developer. Emma is passionate about diversity in tech
+        and in supporting others as they take their steps into tech.
       </p>
     </div>
   );
